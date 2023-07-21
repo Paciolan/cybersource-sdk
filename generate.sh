e3fb8064af1283b3b2a05ae1a4d04240596bf3a6
Louis Orleans <louis@orleans.io>
2023-07-21 16:33:27 -0700

    🐛 fix package.json missing info

--- generate.sh
+++ generate.sh
@@ -7,6 +7,7 @@ set -e
 output_dir="output"
 template_dir="template"
 auth_dir="authentication"
+tmp_dir="/tmp/com.cybersource.node-sdk"
 
 # generate the library
 java -jar swagger-codegen-cli-3.0.43.jar \
@@ -24,6 +25,12 @@ fi
 
 # copy auth files into generated output
 cp -r "$auth_dir/" "$output_dir/$auth_dir/"
+
+# update package.json
+tmp_packagejson="$tmp_dir/package.json"
+mkdir -p "$(dirname "$tmp_packagejson")"
+jq -s '.[0] * .[1]' "$output_dir/package.json" "./package.json" > "$tmp_packagejson"
+mv "$tmp_packagejson" "$output_dir/package.json"
 
 # build the project
 cd "$output_dir"
