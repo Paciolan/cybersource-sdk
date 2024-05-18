9e56bfc888a33814e9f1381f24b7ad1cce354f44
Louis Orleans <lorleans@paciolan.com>
2024-05-17 21:36:25 -0700

    ✨ use github token if available

--- scripts/lib/check-latest-release.sh
+++ scripts/lib/check-latest-release.sh
@@ -4,8 +4,13 @@ function check_latest_release() {
   repo="$1"
   version="$(echo "$2" | sed "$strip_v")"
 
+  cmd="curl --silent 'https://api.github.com/repos/$repo/releases'"
+  if [ -n "$GITHUB_TOKEN" ]; then
+    cmd="$cmd --header 'Authorization: Bearer $GITHUB_TOKEN'"
+  fi
+
   latest_version="$(
-    curl --silent "https://api.github.com/repos/$repo/releases" \
+    eval $cmd \
     | jq --raw-output '. | sort_by(.tag_name) | .[-1].tag_name' \
     | sed "$strip_v"
   )"
